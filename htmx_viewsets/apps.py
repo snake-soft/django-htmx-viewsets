1070c49091a1d81a53a0308a042d1336eef89732
Snake-Soft <info@snake-soft.com>
2023-01-18 00:13:12 +0100

    Remove unused files

--- htmx_viewsets/apps.py
+++
@@ -1,8 +0,0 @@
-from django.utils.translation import gettext_lazy as _
-from django.apps.config import AppConfig
-
-
-class HtmxViewsetsConfig(AppConfig):
-    name = 'htmx_viewsets'
-    verbose_name = _('HTMX Viewsets')
-    namespace = 'htmx_viewsets'
