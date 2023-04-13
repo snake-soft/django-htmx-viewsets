5a27682676ea697f3d83be2a1cedc719bcca14ae
Snake-Soft <info@snake-soft.com>
2023-04-13 13:23:47 +0200

    Remove src from setup because its moved to docker

--- setup.py
+++ setup.py
@@ -41,8 +41,6 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license='GPL3',
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
     include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
